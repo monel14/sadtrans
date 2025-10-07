30ebd8492157e24b8de915c3293acd1589f404e9
monel14 <zomadigilris@gmail.com>
2025-10-07 22:29:45 +0100

    Suppression de l'initialisation du service PWA lors du chargement de l'application

--- index.tsx
+++ index.tsx
@@ -3,16 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
 */
 import { App } from './app';
-import { pwaService } from './services/pwa.service';
 
 document.addEventListener('DOMContentLoaded', () => {
     const rootElement = document.getElementById('app');
     if (rootElement) {
         const app = new App(rootElement);
         app.init();
-        
-        // Initialiser le service PWA
-        pwaService.createInstallButton();
     } else {
         console.error("Root element #app not found.");
     }
